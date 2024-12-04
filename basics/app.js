8c09ab3d7f8462386675b09f64ad3d1bbde4c8db
simplyObare <josephatowiny@gmail.com>
2024-12-04 03:51:11 +0300

    using alpine store and accessing the data

--- basics/app.js
+++ basics/app.js
@@ -5,4 +5,9 @@ document.addEventListener('alpine:init', () => {
       this.open = !this.open
     },
   }))
+
+  Alpine.store('currentUser', {
+    username: 'John Doe',
+    posts: ['Post 1', 'Post 2', 'Post 3'],
+  })
 })
